6c3ff061065228a63c81e3c0454d9acd7892a91f
Ayu21815 <ayushibhargava218@gmail.com>
2022-05-13 19:08:28 +0530

    Server set up

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const express = require('express');
-require('./db/conn');
-const Student = require(".models/Students");
-const app = express();
-const port = process.env.PORT || 8000;
 
-app.use(express.json())
-//create a new student
+const path=require('path')
+const express=require('express')
+const hbs=require('hbs')
 
-app.post('/students' , (req ,res) => {
-    res.send("hello from the other side");
+const app=express()
+const port=process.env.PORT || 3000
 
+//define paths for express config
+const publicpath=path.join(__dirname,'../public')
+const viewspath=path.join(__dirname, '../templates/views')
+const partialspath=path.join(__dirname, '../templates/partials')
+
+//setup handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewspath)
+hbs.registerPartials(partialspath)
+
+//setup static directory to serve
+app.use(express.static(publicpath))
+
+app.listen(port,()=>{
+    console.log('Server is up')
 })
-
-app.listen(port , () => {
-    console.log(`connection is setup at ${port}`);
-})
-
